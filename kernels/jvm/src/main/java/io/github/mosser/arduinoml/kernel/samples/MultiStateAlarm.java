b24d3edfaad3121430f6dce6bdf8fb87340d376d
Pierre-Adrien Vasseur <pierreadrien.vasseur@gmail.com>
2024-11-23 09:14:21 +0100

    feat: add samples scenarios for kernel

---
+++ kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/samples/MultiStateAlarm.java
@@ -0,0 +1,105 @@
+package io.github.mosser.arduinoml.kernel.samples;
+
+import io.github.mosser.arduinoml.kernel.App;
+import io.github.mosser.arduinoml.kernel.behavioral.Action;
+import io.github.mosser.arduinoml.kernel.behavioral.Node;
+import io.github.mosser.arduinoml.kernel.behavioral.SignalTransition;
+import io.github.mosser.arduinoml.kernel.behavioral.State;
+import io.github.mosser.arduinoml.kernel.generator.ToWiring;
+import io.github.mosser.arduinoml.kernel.generator.Visitor;
+import io.github.mosser.arduinoml.kernel.structural.Actuator;
+import io.github.mosser.arduinoml.kernel.structural.SIGNAL;
+import io.github.mosser.arduinoml.kernel.structural.Sensor;
+
+import java.util.Arrays;
+
+public class MultiStateAlarm {
+    public static void main(String[] args) {
+
+        // Declaring elementary bricks
+        Sensor button = new Sensor();
+        button.setName("button");
+        button.setPin(9);
+
+        Actuator led = new Actuator();
+        led.setName("LED");
+        led.setPin(12);
+
+        Actuator buzzer = new Actuator();
+        buzzer.setName("Buzzer");
+        buzzer.setPin(13);
+
+        // Declaring states
+        State initial = new State();
+        initial.setName("initial");
+
+        State buzzerOn = new State();
+        buzzerOn.setName("buzzerOn");
+
+        State ledOn = new State();
+        ledOn.setName("ledOn");
+
+        Action switchTheBuzzerOn = new Action();
+        switchTheBuzzerOn.setActuator(buzzer);
+        switchTheBuzzerOn.setValue(SIGNAL.HIGH);
+
+        Action switchTheBuzzerOff = new Action();
+        switchTheBuzzerOff.setActuator(buzzer);
+        switchTheBuzzerOff.setValue(SIGNAL.LOW);
+
+        Action switchTheLedOn = new Action();
+        switchTheLedOn.setActuator(led);
+        switchTheLedOn.setValue(SIGNAL.HIGH);
+
+        Action switchTheLedOff = new Action();
+        switchTheLedOff.setActuator(led);
+        switchTheLedOff.setValue(SIGNAL.LOW);
+
+        // Binding actions to states
+        initial.setActions(Arrays.asList(switchTheLedOff, switchTheBuzzerOff));
+        buzzerOn.setActions(Arrays.asList(switchTheBuzzerOn));
+        ledOn.setActions(Arrays.asList(switchTheLedOn, switchTheBuzzerOff));
+
+        // Creating transitions
+        SignalTransition initial2buzzerOn = new SignalTransition();
+        initial2buzzerOn.setNext(buzzerOn);
+        Node initial2BuzzerOnCondition = new Node();
+        initial2BuzzerOnCondition.setSensor(button);
+        initial2BuzzerOnCondition.setValue(SIGNAL.LOW);
+        initial2buzzerOn.setCondition(initial2BuzzerOnCondition);
+
+        SignalTransition buzzerOn2LedOn = new SignalTransition();
+        buzzerOn2LedOn.setNext(ledOn);
+        Node buzzerOn2LedOnCondition = new Node();
+        buzzerOn2LedOnCondition.setSensor(button);
+        buzzerOn2LedOnCondition.setValue(SIGNAL.LOW);
+        buzzerOn2LedOn.setCondition(buzzerOn2LedOnCondition);
+
+        SignalTransition ledOn2Initial = new SignalTransition();
+        ledOn2Initial.setNext(initial);
+        Node ledOn2InitialCondition = new Node();
+        ledOn2InitialCondition.setSensor(button);
+        ledOn2InitialCondition.setValue(SIGNAL.LOW);
+        ledOn2Initial.setCondition(ledOn2InitialCondition);
+
+        // Binding transitions to states
+        initial.addTransition(initial2buzzerOn);
+        buzzerOn.addTransition(buzzerOn2LedOn);
+        ledOn.addTransition(ledOn2Initial);
+
+        // Building the App
+        App theAlarm = new App();
+        theAlarm.setName("MultiStateAlarm!");
+        theAlarm.setBricks(Arrays.asList(button, led, buzzer));
+        theAlarm.setStates(Arrays.asList(initial, buzzerOn, ledOn));
+        theAlarm.setInitial(initial);
+
+        // Generating Code
+        Visitor codeGenerator = new ToWiring();
+        theAlarm.accept(codeGenerator);
+
+        // Printing the generated code on the console
+        System.out.println(codeGenerator.getResult());
+    }
+
+}
---
+++ kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/samples/StateBasedAlarm.java
@@ -0,0 +1,81 @@
+package io.github.mosser.arduinoml.kernel.samples;
+
+import io.github.mosser.arduinoml.kernel.App;
+import io.github.mosser.arduinoml.kernel.behavioral.Action;
+import io.github.mosser.arduinoml.kernel.behavioral.Node;
+import io.github.mosser.arduinoml.kernel.behavioral.SignalTransition;
+import io.github.mosser.arduinoml.kernel.behavioral.State;
+import io.github.mosser.arduinoml.kernel.generator.ToWiring;
+import io.github.mosser.arduinoml.kernel.generator.Visitor;
+import io.github.mosser.arduinoml.kernel.structural.Actuator;
+import io.github.mosser.arduinoml.kernel.structural.SIGNAL;
+import io.github.mosser.arduinoml.kernel.structural.Sensor;
+
+import java.util.Arrays;
+
+public class StateBasedAlarm {
+    public static void main(String[] args) {
+
+        // Declaring elementary bricks
+        Sensor button = new Sensor();
+        button.setName("button");
+        button.setPin(9);
+
+        Actuator led = new Actuator();
+        led.setName("LED");
+        led.setPin(12);
+
+        // Declaring states
+        State on = new State();
+        on.setName("on");
+
+        State off = new State();
+        off.setName("off");
+
+        Action switchTheLedOn = new Action();
+        switchTheLedOn.setActuator(led);
+        switchTheLedOn.setValue(SIGNAL.HIGH);
+
+        Action switchTheLedOff = new Action();
+        switchTheLedOff.setActuator(led);
+        switchTheLedOff.setValue(SIGNAL.LOW);
+
+        // Binding actions to states
+        on.setActions(Arrays.asList(switchTheLedOn));
+        off.setActions(Arrays.asList(switchTheLedOff));
+
+        // Creating transitions
+        SignalTransition on2off = new SignalTransition();
+        on2off.setNext(off);
+        Node on2offCondition = new Node();
+        on2offCondition.setSensor(button);
+        on2offCondition.setValue(SIGNAL.LOW);
+        on2off.setCondition(on2offCondition);
+
+        SignalTransition off2on = new SignalTransition();
+        off2on.setNext(on);
+        Node off2onCondition = new Node();
+        off2onCondition.setSensor(button);
+        off2onCondition.setValue(SIGNAL.LOW);
+        off2on.setCondition(off2onCondition);
+
+        // Binding transitions to states
+        on.addTransition(on2off);
+        off.addTransition(off2on);
+
+        // Building the App
+        App theAlarm = new App();
+        theAlarm.setName("StateBasedAlarm!");
+        theAlarm.setBricks(Arrays.asList(button, led));
+        theAlarm.setStates(Arrays.asList(on, off));
+        theAlarm.setInitial(off);
+
+        // Generating Code
+        Visitor codeGenerator = new ToWiring();
+        theAlarm.accept(codeGenerator);
+
+        // Printing the generated code on the console
+        System.out.println(codeGenerator.getResult());
+    }
+
+}
